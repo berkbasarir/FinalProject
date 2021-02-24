8b52aa4632a85c31ffbb4f3be6c55f3213608c72
berkbasarir <bbasarir@gmail.com>
2021-02-25 01:12:11 +0300

    25.02.21 L10.3

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -9,13 +9,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll(); //tümünü getir
-        List<Product> GetAllByCategoryId(int id); //kategori id sine göre tümünü getir
-        List<Product> GetByUniyPrice(decimal min, decimal max); //fiyata göre getir min - max
-        List<ProductDetailDto> GetProductDetails();
-        Product GetById(int productId);
+        IDataResult<List<Product>> GetAll(); //tümünü getir
+        IDataResult<List<Product>> GetAllByCategoryId(int id); //kategori id sine göre tümünü getir
+        IDataResult<List<Product>> GetByUniyPrice(decimal min, decimal max); //fiyata göre getir min - max
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId);
         IResult Add(Product product);
-
-
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -20,21 +21,28 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
+            //business code
 
             if (product.ProductName.Length <2)
             {
-                return new ErrorResult("Ürün ismi en az 2 karakter olmalıdır");
+                //------------magic strings----------
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
             //business codes
             _productDal.Add(product);
-            return new SuccessResult("Ürün Eklendi");
+            return new SuccessResult(Messages.ProductAdded);
         }
 
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
             //İş kodları
             //Yetkisi var mı?
-            return _productDal.GetAll();
+
+            if (DateTime.Now.Hour == 22)
+            {
+                return new ErrorDataResult();
+            }
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), true, "Ürünler Listelendi");
         }
 
         public List<Product> GetAllByCategoryId(int id)
